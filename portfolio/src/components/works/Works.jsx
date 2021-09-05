595e933b5f96120bb35b2dba55b33e8ced90350e
stringedcat <mauroox09@gmail.com>
2021-09-05 05:49:42 -0300

    edits al work

--- portfolio/src/components/works/Works.jsx
+++ portfolio/src/components/works/Works.jsx
@@ -1,11 +1,32 @@
-import "./works.scss"
+import "./works.scss";
 
 const Works = () => {
-    return (
-        <div className="works" id="works">
-            
+  return (
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Exercitationem culpa aliquam eos doloribus eligendi? Quas,
+                  eveniet nobis. Beatae nobis illo voluptatem cum id, inventore
+                  ipsa eum non ratione cupiditate suscipit!
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Works
+export default Works;
