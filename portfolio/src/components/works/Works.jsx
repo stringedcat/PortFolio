7882f1f937d314774ba87277ca6b9ee36ba24636
stringedcat <mauroox09@gmail.com>
2021-09-13 22:58:47 -0300

    update portfolio

--- portfolio/src/components/works/Works.jsx
+++ portfolio/src/components/works/Works.jsx
@@ -23,7 +23,7 @@ const Works = () => {
   ];
   const handleClick = (way) => {
     way === "left"
-      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 1)
       : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
   };
   return (
