fe41942d99de00eda240e3b86d87ffd43b8e677c
stringedcat <mauroox09@gmail.com>
2021-09-20 02:50:52 -0300

    updtaed menu

--- portfolio/src/components/menu/Menu.jsx
+++ portfolio/src/components/menu/Menu.jsx
@@ -41,7 +41,7 @@ const Menu = ({ menuOpen, setMenuOpen }) => {
         </a>
         <a href="https://www.instagram.com/mauro.lyola/" target="_blank">
           <img
-            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png"
+            src="https://www.msm.gov.ar/wp-content/uploads/2020/05/new-instagram-logo-png-transparent-light.png"
             alt="linkedin"
             width="40"
             height="40"
