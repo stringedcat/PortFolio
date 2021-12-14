2835104283510b920ef9367762b8f24ee5a0cab1
stringedcat <mauroox09@gmail.com>
2021-12-14 11:31:30 -0300

    added more data

--- portfolio/src/data.js
+++ portfolio/src/data.js
@@ -1,28 +1,33 @@
-export const featuredPortfolio = [
-  {
-    id: 1,
-    title: "Food Recipes App",
-    img: "https://i.imgur.com/gLTamGD.png",
-    href: "https://github.com/stringedcat/loyolarecipes",
-  },
-];
-
 export const webPortfolio = [
   {
     id: 1,
     title: "Food Recipes App",
     img: "https://i.imgur.com/gLTamGD.png",
+    repo: "https://github.com/stringedcat/loyolarecipes",
   },
   {
     id: 2,
     title: "Lessons Online e-commerce",
     img: "https://user-images.githubusercontent.com/78386531/127190049-39886743-5e39-4282-939a-30e342ed1790.png",
+    repo: "https://github.com/DiegoAraujoJS/PF_E-Commerce",
+  },
+  {
+    id: 3,
+    title: "OAuth App",
+    img: "https://user-images.githubusercontent.com/78386531/127190049-39886743-5e39-4282-939a-30e342ed1790.png",
+    repo: "https://github.com/stringedcat/loginPassport",
+  },
+  {
+    id: 4,
+    title: "MaterialUI Movies",
+    img: "https://user-images.githubusercontent.com/78386531/127190049-39886743-5e39-4282-939a-30e342ed1790.png",
+    repo: "https://github.com/stringedcat/MoviesMaterialUI",
   },
 ];
 export const mobilePortfolio = [
   {
     id: 1,
-    title: "Coming soon",
+    title: "React-Native Coming soon",
     img: "https://thumbs.dreamstime.com/b/work-progress-loading-bar-concept-hand-drawing-work-progress-loading-bar-concept-marker-transparent-wipe-board-167678984.jpg",
   },
 ];
