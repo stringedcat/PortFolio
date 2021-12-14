037e9f5c2668d3b2a45c8505509af875fca5bfd8
stringedcat <mauroox09@gmail.com>
2021-12-14 12:34:11 -0300

    updated data

--- portfolio/src/data.js
+++ portfolio/src/data.js
@@ -14,7 +14,7 @@ export const webPortfolio = [
   {
     id: 3,
     title: "OAuth App",
-    img: "https://user-images.githubusercontent.com/78386531/127190049-39886743-5e39-4282-939a-30e342ed1790.png",
+    img: "https://i.imgur.com/nJzHzjs.png",
     repo: "https://github.com/stringedcat/loginPassport",
   },
   {
